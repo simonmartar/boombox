c05573970a8fe5a385c4669a467c8ecc04fb2641
simonmartar <simonmart@tutamail.com>
2026-07-23 17:41:23 +0300

    Create SampleDocument.kt

---
+++ sample/SampleDocument.kt
@@ -0,0 +1,21 @@
+package sample
+
+val markdown = """
+# Project
+
+## Installation
+
+## Configuration
+
+### Database
+
+### Logging
+
+## Usage
+
+### CLI
+
+### Examples
+
+## License
+""".trimIndent()
